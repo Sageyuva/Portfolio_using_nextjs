3fcc6965ca7a255f25393fc2c2a1ade3954d2d59
Sageyuva <yuvarajdhammure@gmail.com>
2024-04-17 12:12:56 +0530

    updated projects

--- app/Project/page.js
+++ app/Project/page.js
@@ -8,12 +8,13 @@ import textu from "../images/textt.jpg"
 import port from "../images/port.png"
 import flu from "../images/download.png"
 import calcu from "../images/calc.png"
+import chat from "../images/chatting.png"
 const page = () => {
   return (
     <>
     <div className={hs.waste}></div>
     <div className={ps.main}>
-    
+    <Card img={chat} name="Live chatting website" disc="Created full stack chatting website" link="https://github.com/Sageyuva/" />
     <Card img={port} name="Portfolio using Next.Js 13" disc="portfolio using next js 13 and inbuilt app router" link="https://github.com/Sageyuva/Portfolio_using_nextjs" />
         <Card img={imgy} name="Youtube clone using Rapid Api" disc="Youtube clone using Rapid api and React router dom with search bar " link="https://github.com/Sageyuva/youtube_clone_custom" />
         <Card img={news} name="News Api Website using React" disc="Created a news website using api and filters " link="https://github.com/Sageyuva/React-_js_News_api_project" />
